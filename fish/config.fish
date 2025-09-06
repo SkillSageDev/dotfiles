a5455fe29db473be5b8b86793e58dcc5238abdbe
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2025-09-06 11:52:08 +0300

    feat: add neovim plugins and bindings to kitty
    - add autosave plugin
    - add blink plugin for auto completion
    - add fzf-lua plugin to search files
    - add lazygit plugin
    - add liveserver plugin for web development
    - add lspconfig plugin to configure language servers
    - add treesitter plugin for language color highlitghting
    - add ctrl-l key binding to kitty for auto completion for default and vi modes
    - made kitty automatically start in vim mode

--- fish/config.fish
+++ fish/config.fish
@@ -31,3 +31,7 @@ if test -z (pgrep ssh-agent | string collect)
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID
 end
+
+bind ctrl-l -M default accept-autosuggestion
+bind ctrl-l -M insert accept-autosuggestion
+fish_vi_key_bindings insert
